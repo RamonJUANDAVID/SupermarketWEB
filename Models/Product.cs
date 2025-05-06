d42a5b3f2d9066ec5fa6d34532fe929bacbfd763
RamonJUANDAVID <jdramon30@gmail.com>
2025-05-06 00:23:29 -0500

    message

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        //[Key] -> Anotacion si la propiedad no se llama Id, ejemplo ProductId
+        public int Id { get; set; } // será la llave primaria
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }//Será la llave foránea
+        public Category Category { get; set; } //Propiedad de navegación
+    }
+}
