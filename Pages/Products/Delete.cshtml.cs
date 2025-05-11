8d5671011423c942e6566123d92c0cd5fb7d253b
RamonJUANDAVID <jdramon30@gmail.com>
2025-05-10 21:36:33 -0500

    "Commit PayMode"

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Products.FindAsync(id);
+
+            if (category != null)
+            {
+                Product product = null;
+                Product = product;
+                _context.Products.Remove(Product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
